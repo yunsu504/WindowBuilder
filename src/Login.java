0f2c611e4a27144ef9cad7c5118263b8403bfaea
yunsu504 <yunsu504@gmail.com>
2021-01-29 11:20:04 +0900

    Login success

--- src/Login.java
+++ src/Login.java
@@ -8,6 +8,8 @@ import javax.swing.JOptionPane;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 public class Login {
 
 	protected Shell shell;
@@ -18,9 +20,10 @@ public class Login {
 	 * Launch the application.
 	 * @param args
 	 */
+	static Connection connection = null;
+	
 	public static void main(String[] args) {
-		Connection connection = null;
-		
+	
 		try {
 			connection = SqliteConnection.dbConnector();
 			Login window = new Login();
@@ -66,9 +69,38 @@ public class Login {
 		textUserName = new Text(shell, SWT.BORDER);
 		textUserName.setBounds(150, 53, 154, 46);
 		
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.setBounds(72, 180, 232, 39);
-		btnNewButton.setText("New Button");
+		Button btnLogin = new Button(shell, SWT.NONE);
+		btnLogin.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try{
+					String sql = "select * from EmployeeInfo where UserName = ? and Password =?";
+					PreparedStatement pstmt = connection.prepareStatement(sql);
+					pstmt.setString(1,textUserName.getText());
+					pstmt.setString(2,textPassword.getText());
+					System.out.println("before");
+					ResultSet rs = pstmt.executeQuery();
+					System.out.println("after");
+					int count = 0;
+					while (rs.next()){
+						count = count + 1;
+					}
+					if (count == 1){
+						JOptionPane.showMessageDialog(null, "Login Successful");
+					}else if (count > 1){
+						JOptionPane.showMessageDialog(null, "Duplicate UserInfo");
+					}else{
+						JOptionPane.showMessageDialog(null, "Login Faild");
+					}
+					rs.close();
+					pstmt.close();
+				}catch(Exception e1){
+					JOptionPane.showMessageDialog(null, e1);
+				}
+			}
+		});
+		btnLogin.setBounds(72, 180, 232, 39);
+		btnLogin.setText("Login");
 		
 		textPassword = new Text(shell, SWT.BORDER | SWT.PASSWORD);
 		textPassword.setBounds(150, 117, 154, 46);
