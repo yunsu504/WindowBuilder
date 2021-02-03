dd209ffb746fd2a50b63b7dfbfa9a2ebd1920a00
yunsu504 <yunsu504@gmail.com>
2021-02-03 10:22:53 +0900

    Insert DB in JTable

--- src/EmployeeInfo.java
+++ src/EmployeeInfo.java
@@ -4,10 +4,25 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
 import org.eclipse.wb.swt.SWTResourceManager;
 
+import net.proteanit.sql.DbUtils;
+
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+import org.eclipse.swt.SWT;
+
 public class EmployeeInfo {
 
 	protected Shell shell;
-
+	private Table table;
+	
 	/**
 	 * Launch the application.
 	 * @param args
@@ -24,7 +39,17 @@ public class EmployeeInfo {
 	/**
 	 * Open the window.
 	 */
+	
+	private Connection connection = null;
+	private TableColumn tblclmnNewColumn;
+	private TableColumn tblclmnNewColumn_1;
+	private TableColumn tblclmnNewColumn_2;
+	private TableColumn tblclmnNewColumn_3;
+	private TableColumn tblclmnNewColumn_4;
+	
+	private TableItem tableItem;
 	public void open() {
+		connection  = SqliteConnection.dbConnector();
 		Display display = Display.getDefault();
 		createContents();
 		shell.open();
@@ -44,10 +69,62 @@ public class EmployeeInfo {
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
 		
-		Label lblSecondForm = new Label(shell, SWT.NONE);
-		lblSecondForm.setFont(SWTResourceManager.getFont("¸¼Àº °íµñ", 15, SWT.BOLD));
-		lblSecondForm.setBounds(32, 88, 157, 80);
-		lblSecondForm.setText("Second Form");
+		ScrolledComposite scrolledComposite = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite.setBounds(26, 41, 373, 180);
+		scrolledComposite.setExpandHorizontal(true);
+		scrolledComposite.setExpandVertical(true);
+		
+		table = new Table(scrolledComposite, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		tblclmnNewColumn = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn.setWidth(30);
+		tblclmnNewColumn.setText("EID");
+		
+		tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_1.setWidth(60);
+		tblclmnNewColumn_1.setText("Name");
+		
+		tblclmnNewColumn_2 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_2.setWidth(60);
+		tblclmnNewColumn_2.setText("Surname");
+		
+		tblclmnNewColumn_3 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_3.setWidth(50);
+		tblclmnNewColumn_3.setText("Username");
+		
+		tblclmnNewColumn_4 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_4.setWidth(50);
+		tblclmnNewColumn_4.setText("Age");
+		
+		Button btnLoadTable = new Button(shell, SWT.NONE);
+		btnLoadTable.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					String sql = "select * from EmployeeInfo";
+					PreparedStatement pstmt = connection.prepareStatement(sql);
+					ResultSet rs = pstmt.executeQuery();
+					System.out.println(rs.getString(1) + " and "+ rs.getString(2)+ " and "+ rs.getString(3)+ " and "+ rs.getString(4)+ " and "+ rs.getString(5));
+					 
+					while (rs.next()) {
+						tableItem = new TableItem(table, SWT.NONE);
+						tableItem.setText(0, rs.getString(1));
+						tableItem.setText(1, rs.getString(2));
+						tableItem.setText(2,rs.getString(3));
+						tableItem.setText(3,rs.getString(4));
+						tableItem.setText(4,rs.getString(6));
+						scrolledComposite.setContent(table);
+						scrolledComposite.setMinSize(table.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+					    }
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnLoadTable.setBounds(102, 10, 230, 25);
+		btnLoadTable.setText("Load Employee Data");
 
 	}
 }
--- src/SqliteConnection.java
+++ src/SqliteConnection.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+
 import javax.swing.JOptionPane;
 
 import org.eclipse.swt.SWT;
