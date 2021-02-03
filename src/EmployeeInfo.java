d39242aa72f962fd3f9a9ac233202aab094f2384
yunsu504 <yunsu504@gmail.com>
2021-02-03 12:36:48 +0900

    Insert Data

--- src/EmployeeInfo.java
+++ src/EmployeeInfo.java
@@ -17,6 +17,7 @@ import java.sql.*;
 import javax.swing.JOptionPane;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
 
 public class EmployeeInfo {
 
@@ -48,6 +49,14 @@ public class EmployeeInfo {
 	private TableColumn tblclmnNewColumn_4;
 	
 	private TableItem tableItem;
+	private Label lblSetEID;
+	private Label lblSetName;
+	private Label lblSetSurname;
+	private Label lblSetAge;
+	private Text txtSetEID;
+	private Text txtSetName;
+	private Text txtSetSurname;
+	private Text txtSetAge;
 	public void open() {
 		connection  = SqliteConnection.dbConnector();
 		Display display = Display.getDefault();
@@ -70,7 +79,7 @@ public class EmployeeInfo {
 		shell.setText("SWT Application");
 		
 		ScrolledComposite scrolledComposite = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		scrolledComposite.setBounds(26, 41, 373, 180);
+		scrolledComposite.setBounds(161, 41, 238, 180);
 		scrolledComposite.setExpandHorizontal(true);
 		scrolledComposite.setExpandVertical(true);
 		
@@ -90,9 +99,9 @@ public class EmployeeInfo {
 		tblclmnNewColumn_2.setWidth(60);
 		tblclmnNewColumn_2.setText("Surname");
 		
-		tblclmnNewColumn_3 = new TableColumn(table, SWT.NONE);
+		/*tblclmnNewColumn_3 = new TableColumn(table, SWT.NONE);
 		tblclmnNewColumn_3.setWidth(50);
-		tblclmnNewColumn_3.setText("Username");
+		tblclmnNewColumn_3.setText("Username");*/
 		
 		tblclmnNewColumn_4 = new TableColumn(table, SWT.NONE);
 		tblclmnNewColumn_4.setWidth(50);
@@ -113,8 +122,8 @@ public class EmployeeInfo {
 						tableItem.setText(0, rs.getString(1));
 						tableItem.setText(1, rs.getString(2));
 						tableItem.setText(2,rs.getString(3));
-						tableItem.setText(3,rs.getString(4));
-						tableItem.setText(4,rs.getString(6));
+						//tableItem.setText(3,rs.getString(4));
+						tableItem.setText(3,rs.getString(6));
 						scrolledComposite.setContent(table);
 						scrolledComposite.setMinSize(table.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 					    }
@@ -125,6 +134,61 @@ public class EmployeeInfo {
 		});
 		btnLoadTable.setBounds(102, 10, 230, 25);
 		btnLoadTable.setText("Load Employee Data");
+		
+		lblSetEID = new Label(shell, SWT.NONE);
+		lblSetEID.setBounds(0, 69, 56, 15);
+		lblSetEID.setText("EID");
+		
+		lblSetName = new Label(shell, SWT.NONE);
+		lblSetName.setBounds(0, 111, 56, 15);
+		lblSetName.setText("Name");
+		
+		lblSetSurname = new Label(shell, SWT.NONE);
+		lblSetSurname.setBounds(0, 148, 56, 15);
+		lblSetSurname.setText("Surname");
+		
+		lblSetAge = new Label(shell, SWT.NONE);
+		lblSetAge.setBounds(0, 188, 56, 15);
+		lblSetAge.setText("Age");
+		
+		txtSetEID = new Text(shell, SWT.BORDER);
+		txtSetEID.setBounds(48, 63, 99, 21);
+		
+		txtSetName = new Text(shell, SWT.BORDER);
+		txtSetName.setBounds(48, 111, 99, 21);
+		
+		txtSetSurname = new Text(shell, SWT.BORDER);
+		txtSetSurname.setBounds(62, 148, 85, 21);
+		
+		txtSetAge = new Text(shell, SWT.BORDER);
+		txtSetAge.setBounds(48, 182, 99, 21);
+		
+		Button btnSave = new Button(shell, SWT.NONE);
+		btnSave.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					String sql = "Insert into EmployeeInfo (EID,Name,Surname,Age) values (?,?,?,?)";
+					PreparedStatement pstmt = connection.prepareStatement(sql);
+					pstmt.setString(1, txtSetEID.getText());
+					pstmt.setString(2, txtSetName.getText());
+					pstmt.setString(3, txtSetSurname.getText());
+					pstmt.setString(4, txtSetAge.getText());
+					
+					int rs = pstmt.executeUpdate();					 
+					if (rs >= 1 ){
+						JOptionPane.showMessageDialog(null, "Saved Successful");
+						
+					}
+					pstmt.close();
+					
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnSave.setBounds(25, 226, 76, 25);
+		btnSave.setText("SAVE");
 
 	}
 }
